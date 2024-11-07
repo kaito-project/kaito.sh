325219d89c2d4958641d5ce3b63d668ac35c0639
Fei-Guo <vrgf2003@gmail.com>
2024-11-07 09:48:10 -0800

    add docusaurus scaffold

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/guofei/go/src/github.com/kaito-project/kaito.sh/node_modules/infima/dist/css/default/default.css"),
+  require("/home/guofei/go/src/github.com/kaito-project/kaito.sh/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/guofei/go/src/github.com/kaito-project/kaito.sh/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/guofei/go/src/github.com/kaito-project/kaito.sh/src/css/custom.css"),
+];
