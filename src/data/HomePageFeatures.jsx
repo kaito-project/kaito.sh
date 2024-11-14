7025d291d2bfad6f87c777df2ac995b082622f03
sdesai345 <desaisachi345@gmail.com>
2024-11-14 06:51:26 -0800

    Updated feature descriptions and removed fourth section

--- src/data/HomePageFeatures.jsx
+++ src/data/HomePageFeatures.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const features = [
     {
         title: <p>AI model onboarding made easy</p>,
-        imgUrl: 'img/kaito-inference-adapter.png',
+        imgUrl: 'img/kaito-easy-model-deployment.png',
         description: (
             <p>
                 KAITO differs from mainstream model deployment options built on VM infrastructures by managing large model files using container images, providing preset configurations to avoid workload parameter adjustment based on GPU hardware, auto-provisioning GPU nodes based on model requirements, and hosting large OSS model images in a public container registry. Your organization may have specific model requirements for language, performance, versioning, etc. so KAITO helps you easily containerize and onboard a custom model by following a <a href='https://github.com/kaito-project/kaito/docs/custom-model-integration/custom-model-integration-guide.md'>simple 5-step guidance</a>.
@@ -12,34 +12,24 @@ const features = [
         reverse: false,
     },
     {
-        title: <p>Infra-level model performance optimization</p>,
-        imgUrl: 'img/kaito-fine-tuning.png',
+        title: <p>Automate fine-tuning and advanced inference</p>,
+        imgUrl: 'img/kaito-inference-with-tuning-adapters.png',
         description: (
             <p>
-                KAITO implements various techniques to optimize memory management for inferencing and tuning language models. Supported LLM serving engines are high throughput and memory-efficient to reduce response time for user requests at scale. In addition to low-rank adaptation (LoRA), KAITO offers parameter-efficient fine-tuning (PEFT) like quantized low-rank adaptation (QLoRA) for greater memory efficiency while supporting longer sequence (input) length. KAITO supplements these PEFT options with cache clearing and expandable segment by default to reduce GPU reserved memory size and lower the frequency of out-of-memory (OOM) errors. 
+                KAITO streamlines parameter-efficient fine-tuning (PEFT) methods on Kubernetes, like low-rank adaptation (LoRA) and quantized low-rank adaptation (QLoRA) for efficient memory utilization while supporting longer sequence (input) length. Simply provide the input data and tuning method to customize a supported model to your specific context, and KAITO will optimize the Kubernetes deployment with right-size infrastructure while lowering the frequency of out-of-memory (OOM) errors. KAITO also keeps your open-source AI pipelines up-to-date with advanced inference using fine-tuning adapters built from new data in your applications.
             </p>
         ),
         reverse: true,
     },
     {
-        title: <p>Maintain data residency with cloud and edge deployment flexibility</p>,
-        imgUrl: 'img/kaito-fine-tuning.png',
+        title: <p>Deploy models out-of-box with simple presets to bootstrap your AI workload</p>,
+        imgUrl: 'img/kaito-ai-bootstrapping.png',
         description: (
             <p>
-                KAITO integrates a Kubernetes controller that can select GPU nodes across clouds and on the edge to ensure that the AI deployments follow your data securiy standards. Whether your organization requires data residency within a single cloud region or within edge nodes across self-managed facilities and data centers, KAITO offers you the extensibility to "prefer" any GPU node (having the device software components pre-installed). 
+                KAITO simplifies the model and resource configuration process with just a few lines of YAML code! Start with a default model preset and deploy the workspace onto your Kubernetes cluster, while KAITO bootstraps the GPU node setup and inference service deployment behind the scenes. Using the KAITO workspace CRD, you can update to a different GPU type or prefer your own edge nodes to scale out the AI workload for your needs.
             </p>
         ),
         reverse: false,
-    },
-    {
-        title: <p>Built from open-source to plug into your AI pipelines</p>,
-        imgUrl: 'img/kaito-fine-tuning.png',
-        description: (
-            <p>
-                KAITO simplifies inferencing and fine-tuning deployments using the open-source workspace controller and Karpenter node provisioning components. As a Kubernetes operator, KAITO streamlines your Day 0/Day 1 ML operations while also plugging into your end-to-end AI pipeline that supports Day 2 operations like data ingestion, monitoring, alerting, storage, etc.
-            </p>
-        ),
-        reverse: true,
     },
 ]
 export default features
