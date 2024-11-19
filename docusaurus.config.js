f78d98fbd087cc7382a632f83513c0d9c6af47d4
sdesai345 <desaisachi345@gmail.com>
2024-11-19 13:57:58 -0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'kaito-project', // Usually your GitHub org/user name.
   projectName: 'kaito.sh', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'throw',
